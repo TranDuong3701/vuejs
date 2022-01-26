d94ffb0d805b44e32ff664dd5566a78e8de23050
Trần Khắc Bình Dương <tranduong3701@gmail.com>
2022-01-26 08:29:26 +0700

    updated events final code

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      counter: 0,
-    };
-  },
+    data() {
+        return {
+            counter: 10,
+            name: "",
+            actualName: "",
+        };
+    },
+    methods: {
+        add() {
+            this.counter++;
+        },
+        reduce() {
+            this.counter--;
+        },
+        setName(e, lastName = "Tran") {
+            this.name = e.target.value + " " + lastName;
+        },
+        submitForm(e) {
+            // e.preventDefault();
+            alert("OK!");
+        },
+        setActualName() {
+            this.actualName = this.name;
+        },
+    },
 });
 
-app.mount('#events');
+app.mount("#events");
+
+/** Terms & Notes
+ * v-on:click => v-on directive => set event
+ * v-on:input => change input text like onChange
+ * v-on:submit
+ * event modifier => v-on:submit.prevent, v-on:keyup.enter,
+ * v-once => lock the content
+ */
