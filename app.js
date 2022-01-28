d0b230a3dc95e3a41242f7f2098d32eb07d33b8c
Trần Khắc Bình Dương <tranduong3701@gmail.com>
2022-01-28 10:58:35 +0700

    added final code list cond

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const app = Vue.createApp({
-  data() {
-    return { goals: [] };
-  },
+    data() {
+        return {
+            inputValue: "",
+            goals: [],
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.inputValue);
+            this.inputValue = "";
+        },
+        removeGoal(index) {
+            this.goals.splice(index, 1);
+        },
+    },
 });
 
-app.mount('#user-goals');
+app.mount("#user-goals");
+
+/**
+ * v-if, v-else-if, v-else
+ * v-if => remove element in dom
+ * v-show => hidden element by css style
+ */
